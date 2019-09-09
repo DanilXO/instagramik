46e4df56dcc6218fa2885e00bd30781c9619c225
Frolov Daniil <d.frolov@directline.digital>
2019-09-09 11:15:25 +0400

    4 занятие добавлены недостающие шаблоны

--- core/urls.py
+++ core/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     url(r'^post/(?P<post_id>[0-9]+)/$', views.PostView.as_view(), name='post_detail'),
     # Второй вариант
     path('post/<int:post_id>/edit/', login_required(views.EditPostView.as_view()), name='post_edit'),
-    path('post/<int:post_id>/like/', login_required(views.LikePostView.as_view()), name='like-post'),
-    path('post/<int:post_id>/delete/', login_required(views.DeletePostView.as_view()), name='delete-post'),
+    path('post/<int:post_id>/like/', login_required(views.LikePostView.as_view()), name='like_post'),
+    path('post/<int:post_id>/delete/', login_required(views.DeletePostView.as_view()), name='delete_post'),
     path('post/<int:post_id>/delete_success', TemplateView.as_view(template_name='core/delete_success.html'),
          name='delete-post-success'),
     path('post/create/', views.CreatePostView.as_view(), name='post_create'),
