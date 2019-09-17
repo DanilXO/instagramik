13cc4c5b942627c03405bfdd9003d4a45a020ae2
Frolov Daniil <d.frolov@directline.digital>
2019-09-17 08:22:10 +0400

    5е занятие

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.views.generic import TemplateView
 
 from core import views
+from core.views import SignupView, EditProfileView, AddRemoveFriend, ProfileView, logout_view, LoginView
 
 app_name = 'core'
 urlpatterns = [
@@ -18,5 +19,29 @@ urlpatterns = [
          name='delete-post-success'),
     path('post/create/', views.CreatePostView.as_view(), name='post_create'),
     path('feed/', views.FeedView.as_view(), name='feed'),
+]
+
+from django.contrib.auth.views import PasswordResetDoneView, PasswordResetView, PasswordResetConfirmView, \
+    PasswordResetCompleteView
+
+urlpatterns += [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('<int:user_id>/profile/', ProfileView.as_view(), name='profile'),
+    path('<int:user_id>/profile/add_remove_friend', login_required(AddRemoveFriend.as_view()),
+         name='add-remove-friend'),
+    path('<int:user_id>/profile/edit', login_required(EditProfileView.as_view()), name='edit-profile'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('password_reset/', PasswordResetView.as_view(success_url=reverse_lazy('users:password_reset_done'),
+                                                      template_name='my_auth/password_reset.html'),
+         name='password_reset'),
+
+    path('password_reset/done', PasswordResetDoneView.as_view(template_name='my_auth/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('password_reset/<str:uidb64>/<slug:token>', PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('users:password_reset_complete')), name='password_reset_confirm'),
+
+    path('password_reset/complete', PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,17 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
-from django.http import HttpResponse, Http404
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.utils import timezone
-from django.core import exceptions
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView, View
 from django.views.generic import ListView, View, DetailView, CreateView, UpdateView, DeleteView
 from core import models
 from core.exceptions import PermissionDenied
-from core.forms import PostForm, CommentForm
-from core.models import Post
+from core.forms import PostForm, CommentForm, LoginForm, SignupForm, UpdateProfileForm
+from core.models import Post, Profile
 
 
 class IndexView(ListView):
@@ -135,4 +133,99 @@ class DeletePostView(DeleteView):
         post_id = self.kwargs['post_id']
         return reverse('core:delete-post-success', args=(post_id,))
 
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import authenticate, login, logout
+
+
+class LoginView(LoginView):
+    template_name = 'my_auth/login.html'
+    form_class = LoginForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('blog:index'), request)
+            else:
+                context = {}
+                context['form'] = form
+                return render(request=request, template_name=self.template_name, context=context)
+        else:
+            context = {'form': form}
+            return render(request=request, template_name=self.template_name, context=context)
+
+
+class ProfileView(DetailView):
+    model = Profile
+    template_name = 'core/profile.html'
+
+    def get_object(self):
+        return get_object_or_404(Profile, user__id=self.kwargs['user_id'])
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect(reverse("core:index"))
+
+
+class SignupView(View):
+    template_name = 'my_auth/signup.html'
+    registration_form = SignupForm
+
+    def get(self, request, *args, **kwargs):
+        context = {'form': self.registration_form}
+        return render(request=request, template_name=self.template_name, context=context)
+
+    def post(self, request, *args, **kwargs):
+        user_form = SignupForm(data=request.POST)
+        registered = False
+        if user_form.is_valid():
+            user = user_form.save(commit=True)
+            user.email = user_form.cleaned_data['email']
+            user.save()
+            registered = True
+            return render(request, 'my_auth/signup.html',
+                          {'registered': registered})
+        else:
+            return render(request, 'my_auth/signup.html',
+                          {'form': user_form,
+                           'registered': registered})
+
+
+class EditProfileView(UpdateView):
+    model = Profile
+    form_class = UpdateProfileForm
+    template_name = 'core/edit_profile.html'
+    slug_field = "user_id"
+    slug_url_kwarg = "user_id"
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.user != self.request.user:
+            raise Http404("It is not your profile!")
+        return super(EditProfileView, self).dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        user_id = self.kwargs['user_id']
+        return reverse('users:profile', args=(user_id,))
+
+
+class AddRemoveFriend(View):
+
+    def post(self, request, user_id, *args, **kwargs):
+        profile = get_object_or_404(Profile, user__id=user_id)
+        if profile.friends.filter(user__id=request.user.id).exists():
+            friend = profile.friends.get(user__id=request.user.id)
+            profile.friends.remove(friend)
+            request.user.user_profile.friends.remove(profile)
+        else:
+            profile.friends.add(request.user.user_profile)
+            request.user.user_profile.friends.add(profile)
+        return redirect(request.META.get('HTTP_REFERER'), request)
 
